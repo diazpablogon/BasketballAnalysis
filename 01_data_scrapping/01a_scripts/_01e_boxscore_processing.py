6b8100f64bd62f45d1e32cba020ebe3da40939db
diazpablogon <diazpablogon@gmail.com>
2025-10-22 00:18:36 +0200

    Filtra boxscores por equipos NBA

--- 01_data_scrapping/01a_scripts/_01e_boxscore_processing.py
+++ 01_data_scrapping/01a_scripts/_01e_boxscore_processing.py
@@ -415,6 +415,23 @@ def main() -> None:
 
     logging.info("Leyendo boxscores desde %s", args.boxscores)
     boxscores_df = ensure_string_keys(read_parquet(args.boxscores), KEY_COLUMNS)
+    # --- FILTRO DE EQUIPOS NBA ---
+    nba_team_ids = [
+        "1610612737", "1610612738", "1610612739", "1610612740", "1610612741",
+        "1610612742", "1610612743", "1610612744", "1610612745", "1610612746",
+        "1610612747", "1610612748", "1610612749", "1610612750", "1610612751",
+        "1610612752", "1610612753", "1610612754", "1610612755", "1610612756",
+        "1610612757", "1610612758", "1610612759", "1610612760", "1610612761",
+        "1610612762", "1610612763", "1610612764", "1610612765", "1610612766",
+        "1610612767", "1610612768"
+    ]
+
+    before_rows = len(boxscores_df)
+    boxscores_df = boxscores_df[boxscores_df["TEAM_ID"].isin(nba_team_ids)].copy()
+    after_rows = len(boxscores_df)
+
+    print(f"Filtrado equipos no NBA: {before_rows} -> {after_rows} filas")
+    # --- FIN FILTRO ---
     logging.info(
         "Boxscores: %d filas, %d columnas",
         len(boxscores_df),
